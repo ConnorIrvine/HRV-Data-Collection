8a6d7a64421397c88bdf83fc40891cf92057a898
= <=>
2025-11-14 18:00:51 -0500

    fix sensor not detecting data

--- src/PulseSensor_StarterProject_Arduino.cpp
+++ src/PulseSensor_StarterProject_Arduino.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+
 /*  PulseSensor™ Starter Project   http://www.pulsesensor.com
  *   
 This an Arduino project. It's Best Way to Get Started with your PulseSensor™ & Arduino. 
@@ -9,41 +10,48 @@ This an Arduino project. It's Best Way to Get Started with your PulseSensor™ &
 4) This is the direct Pulse Sensor's Signal.  
 5) A great first-step in troubleshooting your circuit and connections. 
 6) "Human-readable" code that is newbie friendly." 
+
+
+ time_t getTime();       // Returns the current time in CET
+ time_t getUTCTime();    // Returns the current time in UTC
+ Start();                // Start listening to DCF77 signal
+ Stop();
+
 */
 
+
 //  Variables
-int PulseSensorPurplePin = 1;        // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
+int PulseSensorPurplePin = 0;        // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
 int LED13 = 13;   //  The on-board Arduion LED
 
 
 int Signal;                // holds the incoming raw data. Signal value can range from 0-1024
-int Threshold = 550;            // Determine which Signal to "count as a beat", and which to ingore. 
-
+int Threshold = 700;            // Determine which Signal to "count as a beat", and which to ingore. 
+int count = 0;
 
 // The SetUp Function:
 void setup() {
   pinMode(LED13,OUTPUT);         // pin that will blink to your heartbeat!
    Serial.begin(9600);         // Set's up Serial Communication at certain speed. 
+  
 }
 
 // The Main Loop Function
 void loop() {
-
-  Signal = analogRead(PulseSensorPurplePin);  // Read the PulseSensor's value.
-  Serial.print(">");
-  Serial.print("LOWER:");
-  Serial.print(300);               // Send the Threshold value to Serial Plotter.
-  Serial.print(",");
-  Serial.print("UPPER:");
-  Serial.print(700);               // Send the Upper-Limit value to Serial Plotter
-  Serial.print(",");
-  Serial.print("Signal:"); 
+  
+  Signal = analogRead(PulseSensorPurplePin);  // Read the PulseSensor's value. 
+  
+  Serial.print('>');
+  Serial.print("Signal:");
   Serial.print(Signal);                    // Send the Signal value to Serial Plotter.
-  Serial.print(",");
+  Serial.print(",lower:");
+  Serial.print(700); // To freeze the lower limit
+  Serial.print(",upper:");
+  Serial.print(300); // To freeze the lower limit
   Serial.println();
-   
+  
   if(Signal > Threshold){                          // If the signal is above "550", then "turn-on" Arduino's on-Board LED.  
-    digitalWrite(LED13,HIGH);          
+    digitalWrite(LED13,HIGH);      
   } else {
     digitalWrite(LED13,LOW);                //  Else, the sigal must be below "550", so "turn-off" this LED.
   }
@@ -53,7 +61,3 @@ delay(10);
    
    
 }
-
-
-
-
